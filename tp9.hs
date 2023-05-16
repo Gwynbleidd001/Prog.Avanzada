8e67887511b8d4f27e05fc66a5c15ed897c0ace7
Gwynbleidd001 <genarooviedo6@gmail.com>
2023-05-16 17:55:58 -0300

    Avancé hasta actividad 4. Rehacer 4

--- tp9.hs
+++ tp9.hs
@@ -37,10 +37,67 @@ prodPrims.xs = <∏i: 0 <= i < #xs && divs (xs.i) = 2: xs.i>
 	where divs: Int -> Int
 	      divs.x = <Contatoria i: 1 <= i <=x && mod i x = 0: i>
 
---ACT. 2
+--ACT.2
 --Sea xs un lista no vacía con elementos booleanos, tal que true aparezca al menos una vez en la lista. Especificar:
 
+--CONTROLAR EN TODAS SI EL TÉRMINO ES CORRECTO
 --n es el menor entero tal que xs.n = true.
+menor : [Int] -> Int
+menor.xs = <Min i: 0 <= i <= #xs && xs.i = True: i>
+
+--n es el último elemento de la lista tal que xs.n = true.
+ult : [Int] -> Int
+ult.xs = <∃i: 0 <= i <= #xs && i = #xs: xs.i>
+ult.xs = <Max i: 0 <= i <= #xs && xs.i: i>
+
+--REVISAR
+--f es una función que devuelve true si y solo si todos los elementos de xs son equivalentes.
+allEq : [Int] -> Bool
+allEq.xs = <∀i,j: 0 <= i <= #xs && 0 <= j < #xs: xs.i = xs.j>
+
+--ACT.3
+--REVISAR
+--f.xs determina si xs tiene la misma cantidad de pares que impares.
+paresImprs: [Int] -> Bool
+paresImprs.xs = <∀i:i = #xs-1 && (cantPares xs.i = cantImpares xs.i): xs.i> 
+paresImprs.xs = (<∏i: 0 <= i <= #xs && mod xs.i 2 = 0: xs.i>) = (<∏i: 0 <= i <= #xs && mod xs.i 2 /= 0: xs.i>)
+
+cantPares : [Int] -> Int
+<∏i: 0 <= i <= #xs && mod xs.i 2 = 0: xs.i>
+
+cantImpares : [Int] -> Int
+<∏i: 0 <= i <= #xs && mod xs.i 2 /= 0: xs.i>
+
+--f.n determina si n es primo.
+prime: Int -> Bool
+prime.n = (<Contatoria i: 1 <= i <= n && mod i x = 0: i> == 2)
+prime.n = (<Contatoria i: 1 <= i <= n: mod i x = 0> == 2)
+
+SE FILTRA EL TÉRMINO
+--f.xs.ys determina si ys es una subsecuencia de xs.
+subsec : [a] -> [a] -> Bool
+subsec.xs.ys = <∃as,bs : xs = as ++ ys ++ bs>
+subsec.xs.ys = <∃as,bs: BUSCAR OTRA FORMA
+
+SE FILTRA EL TÉRMINO
+--f.xs.ys determina si ys es una subsecuencia final de xs.
+subsecFin : [a] -> [a] -> Bool
+subsecFin.xs.ys = <∃as : xs = as ++ ys>
+
+--ACT.4
+--Dada una lista de enteros, especifique la suma del subsegmento de suma mínima de la lista. Por ejemplo, si la lista es xs = [1, −4, −2, --1, −5, 8, −7] el subsegmento que da la suma mínima es [−4, −2, 1, −5], cuya suma es -10. Si xs = [1, 3, 5] , 
+--el subsegmento que da la suma mínima es [] , pues la suma de la lista vacía es cero.
+
+REHACERLA SOLO
+sumMin : [Int] -> 
+sumMin.xs = <Min as,bs,cs: as ++ bs ++ cs == xs: <SUMATORIA i: i <= 0 < #xs: xs.i> >
+
+--Especifique la funcin maxigual que determina la longitud del m ́aximo sub-segmento en donde todos sus elementos son iguales: maxigual : [A] −> Num.
+REHACERLA SOLO
+
+
+
+--Especifique la funci ́on maxdistinto : [Int]− > Int que determina la longitud del subsegmento m ́as largo en donde todos los elementos son distintos.
 
 
 
